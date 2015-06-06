1b5a07afbcff9bf3b591c1a9915e3d704419beb7
Jesse Black <blackjes@gmail.com>
2015-06-05 22:01:21 -0700

    Add src directory. Move node-directory-tools.js update src directory. Add lib directory under src

---
+++ src/node-directory-tools.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+//Define Node Native Modules
+
+//Define Third-Party Modules
+
+(function (){
+	"use strict";
+	//var commandLineArguments = process.argv.slice(2);
+    //
+	//console.log(commandLineArguments);
+	console.log('Hello World');
+})();
